c6480c4fdc92d6e14c90ed5cdc3247f04df82ff4
zuber57 <zubarahmad48@gmail.com>
2025-09-19 11:55:30 +0530

    --

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -70,7 +70,7 @@ const Projects = () => {
               whileHover={{ scale: 1.05 }}
               className="bg-white p-6 rounded-lg shadow-lg"
             >
-              <h3 className="text-2xl font-bold mb-4">{project.title}</h3>
+              <h3 className="text-2xl text-[#000] font-bold mb-4">{project.title}</h3>
               <p className="text-gray-700 mb-4">{project.description}</p>
               <a
                 href={project.link}
